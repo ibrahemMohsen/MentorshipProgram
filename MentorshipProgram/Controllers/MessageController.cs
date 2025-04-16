7a5aa1783614ec29c28987b290db9e280590ed1b
ibrahemMohsen <ibrahemmohsen851@gmail.com>
2025-04-16 11:04:49 +0200

    Added message view

---
+++ MentorshipProgram/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MentorshipProgram.Controllers;
+
+public class MessageController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
