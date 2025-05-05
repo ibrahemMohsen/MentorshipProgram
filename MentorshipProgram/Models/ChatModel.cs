3540ef5cc74442861f3b16ce4d4d1979dd4c9b7d
ibrahemMohsen <ibrahemmohsen851@gmail.com>
2025-05-05 08:45:46 +0300

    Added tags to ChatModel and Message Model

--- MentorshipProgram/Models/ChatModel.cs
+++ MentorshipProgram/Models/ChatModel.cs
@@ -7,8 +7,12 @@ public class ChatModel
     [Key]
     public int Id { get; set; }
     [Required]
+    [EmailAddress]
+    [Length(8, 100)]
     public string Participant1Email { get; set; }
     [Required]
+    [EmailAddress]
+    [Length(8, 100)]
     public string Participant2Email { get; set; }
 
     public virtual ICollection<MessageModel> Messages { get; set; } = new List<MessageModel>();
--- MentorshipProgram/Models/MessageModel.cs
+++ MentorshipProgram/Models/MessageModel.cs
@@ -8,8 +8,12 @@ public class MessageModel
     [Key]
     public int Id { get; set; }
     [Required]
+    [EmailAddress]
+    [Length(8, 100)]
     public string SenderEmail { get; set; }
     [Required]
+    [EmailAddress]
+    [Length(8, 100)]
     public string ReceiverEmail { get; set; }
     [Required]
     public string Body { get; set; }
